f4b7890f4503bdeb4c58716e82813324bb1a4d1c
hunter <hunterjsb@gmail.com>
2023-12-10 14:10:22 -0500

    i have to so close

--- src/adventofcode2023/day4/main.go
+++ src/adventofcode2023/day4/main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 )
+
+func toInt(a byte, b byte) int {
+	if a < '0' {
+		return int(b - '0')
+	}
+	return int(a-'0')*10 + int(b-'0')
+}
 
 func main() {
 	fmt.Println("Starting main...")
-	// var fd, ld int
 	var buf [117]byte // 116 characters per line, plus one for newline
+	var sum, matches int
+	nums := make([]int, 0)
 
 	file, _ := os.Open("input.txt")
 	defer file.Close()
@@ -16,9 +25,22 @@ func main() {
 	for y := 0; y < 220; y++ {
 		file.Read(buf[:])
 		fmt.Print(y, ": ")
-		for _, b := range buf[10:39] {
-			fmt.Printf("%c", b)
+		for i := 10; i < 39; i += 3 {
+			nums = append(nums, toInt(buf[i], buf[i+1]))
+		}
+		fmt.Print(nums, " ... ")
+		for i := 42; i < 115; i += 3 {
+			fmt.Printf("%c%c ", buf[i], buf[i+1])
+			n := toInt(buf[i], buf[i+1])
+			if slices.Contains(nums, n) {
+				sum += 1 << matches
+				matches++
+				fmt.Printf("(%v) [+%v(%v) = %v]  ", n, 1<<(matches-1), matches, sum)
+			}
 		}
 		fmt.Print("\n")
+		nums = nil
+		matches = 0
 	}
+	fmt.Println("SUM: ", sum)
 }
