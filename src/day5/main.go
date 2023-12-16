5088e40c56d0897a8018ec03fb4c38fe62b72028
hunter <hunterjsb@gmail.com>
2023-12-15 23:49:08 -0500

    correlation map... finally

--- src/day5/main.go
+++ src/day5/main.go
@@ -8,34 +8,59 @@ import (
 
 // don't forget newline before eof
 const filename string = "test_input.txt"
+const max int = 99
 
 func isNumeric(b byte) bool {
 	return b >= '0' && b <= '9'
 }
 
 func main() {
-	buf := make([]byte, 1)
-	num := make([]byte, 0)
-	// seeds := make([]int, 0)
+	// highestSeed := 0
+	// lowestNum := max
+
+	fileBuf := make([]byte, 1)
+	digitBuf := make([]byte, 3)
+	m := make(map[int]int)
 
 	file, _ := os.Open(filename)
 	defer file.Close()
 
-	for i := 0; i < 33; i++ {
+	parseLine := func() []int {
+		s := make([]int, 0)
 		for {
-			file.Read(buf)
-			if isNumeric(buf[0]) {
-				num = append(num, buf[0])
-			} else if buf[0] == 32 && len(num) > 0 {
-				n, _ := strconv.Atoi(string(num))
-				fmt.Printf(" [[%v]] ", n)
-				num = nil
-			} else if buf[0] == 10 {
-				n, _ := strconv.Atoi(string(num))
-				fmt.Printf(" [{%v}]\n", n)
-				num = nil
+			file.Read(fileBuf)
+			if isNumeric(fileBuf[0]) {
+				digitBuf = append(digitBuf, fileBuf[0])
+			} else if fileBuf[0] == ' ' && len(digitBuf) > 0 {
+				n, _ := strconv.Atoi(string(digitBuf))
+				s = append(s, n)
+				digitBuf = nil
+			} else if fileBuf[0] == '\n' {
+				n, _ := strconv.Atoi(string(digitBuf))
+				s = append(s, n)
+				digitBuf = nil
 				break
 			}
 		}
+		if len(s) > 1 {
+			return s
+		}
+		return nil
+	}
+
+	seeds := parseLine()[1:]
+	fmt.Println("Seeds: ", seeds)
+	for i := 1; i < 33; i++ {
+		v := parseLine()
+		if v == nil {
+			fmt.Println("eos")
+			clear(m)
+			continue
+		}
+		fmt.Printf("%v\n", v)
+		for j := 0; j < v[2]; j++ {
+			m[v[1]+j] = v[0] + j
+		}
+		fmt.Println(m)
 	}
 }
